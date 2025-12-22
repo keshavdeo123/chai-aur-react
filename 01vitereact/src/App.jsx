6fe141ef60d3b46d02711f2ee536e914bd28dd42
Keshav Deo <keshavmaharaj345@gmail.com>
2025-12-22 16:31:44 +0530

    01vitereact

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import Chai from './abc.jsx'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (<>
+  <Chai/>
+  <h2>Hellow hru</h2>
+  </>
+   
+   
+  )
+}
+
+export default App
